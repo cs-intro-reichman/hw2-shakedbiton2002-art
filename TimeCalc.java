c3fbd4720b725bb4c04d7a65703f4e8023482995
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-11-07 21:05:37 +0200

    test 4

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,21 +1,23 @@
 public class TimeCalc {
     public static void main(String[] args) {
         String[] parts = args[0].split(":");
-		int hours = Integer.parseInt(parts[0]);
-		int minutes = Integer.parseInt(parts[1]);
-		int minutesadd = Integer.parseInt(args[1]);
-		int Total = (hours * 60 + minutes + minutesadd);
-		int newhours = (Total / 60) % 24;
-		int newminutes = Total % 60;
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+        int minutesadd = Integer.parseInt(args[1]);
+
+        int total = hours * 60 + minutes + minutesadd;
+        int newhours = (total / 60) % 24;
+        int newminutes = total % 60;
         if (newhours < 10) {
             System.out.print("0" + newhours + ":");
-         } else {
+        } else {
             System.out.print(newhours + ":");
-         }
+        }
         if (newminutes < 10) {
-            System.out.println(newhours + ":0" + newminutes);
-         } else { 
-            System.out.println(newhours + ":" + newminutes);
-   }
+            System.out.println("0" + newminutes);
+        } else {
+            System.out.println(newminutes);
+        }
+    }
 }
-}
+
