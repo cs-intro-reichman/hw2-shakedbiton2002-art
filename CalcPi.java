fd9aa55928868ce5751b3b2271926486c86ed80e
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-11-07 20:34:57 +0200

    test 1

--- CalcPi.java
+++ CalcPi.java
@@ -10,7 +10,7 @@ public class CalcPi {
 				Estimate = Estimate - 1.0 / (2 * x + 1);
 			}	
 	}
-	System.out.println("pi according to Java:" + Math.PI);
+	System.out.println("pi according to Java: " + Math.PI);     
 	System.out.println("pi, approximated:" + 4 * Estimate);
 }
 
