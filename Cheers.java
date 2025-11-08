17384ee44cbe8005f93c36aa368599d992a88bd0
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-11-08 10:57:55 +0200

    test 5

--- Cheers.java
+++ Cheers.java
@@ -3,19 +3,22 @@ public class Cheers {
     public static void main(String[] args) {
         String name = args[0];
         int times = Integer.parseInt(args[1]);
+        String upperName = name.toUpperCase(); 
 
-        for (int i = 0; i < name.length(); i++) {
-                 char c = name.charAt(i);
-                 if ("AEFHILMNORSX".indexOf(c) >= 0) {
-                         System.out.println("Give me an " + c + ": " + c + "!");
-                        } else {
-                                System.out.println("Give me a " + c + ": " + c + "!"); }
+        for (int i = 0; i < upperName.length(); i++) {
+            char c = upperName.charAt(i);
+            if ("AEFHILMNORSX".indexOf(c) >= 0) {
+                System.out.println("Give me an " + c + ": " + c + "!");
+            } else {
+                System.out.println("Give me a  " + c + ": " + c + "!");
+            }
         }
-        System.out.println("What does that spell?");
 
+        System.out.println("What does that spell?");
         for (int i = 0; i < times; i++) {
-            System.out.println(name + "!!!");
+            System.out.println(upperName + "!!!");
         }
     }
 }
+
 
