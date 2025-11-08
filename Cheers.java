94dbe765e565c98dbe83e61c057fe7409b2e5091
Shaked Biton <shaked.biton@post.runi.ac.il>
2025-11-08 10:49:00 +0200

    test 5

--- Cheers.java
+++ Cheers.java
@@ -1,18 +1,23 @@
 
 public class Cheers {
-        public static void main(String[] args) {
-                String Word = args[0];
-                int n = Integer.parseInt(args[1]);
-                for (int i=0; i< Word.length(); i++) {
-                        char letter = Word.charAt(i);
-                        System.out.println("Give me a " + letter + " :" + letter + "!");
-                }
-                System.out.println("What does that spell? ");
-                for(int j=0; j<n; j++) {
-                        System.out.println( Word + "!");
-                        
+    public static void main(String[] args) {
+        String name = args[0];
+        int times = Integer.parseInt(args[1]);
 
-                }
-	    
+        for (int i = 0; i < name.length(); i++) {
+                 char c = name.charAt(i);
+                  if ("AEIOUaeiou".indexOf(c) >= 0) {
+                         System.out.println("Give me an " + c + ": " + c + "!");
+                         } else {
+                                 System.out.println("Give me a " + c + ": " + c + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < times; i++) {
+            System.out.println(name + "!!!");
+        }
+    }
 }
+
